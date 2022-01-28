f88045b6f7c8e1b55c8666d0f71aeb02f0e8a0a6
Ian S <iansilber32@gmail.com>
2022-01-28 17:04:48 -0500

    working on a solution

--- algo-ladder/Two-Pointers/merge_sorted_arrays.py
+++ algo-ladder/Two-Pointers/merge_sorted_arrays.py
@@ -5,3 +5,17 @@
 # B : [6, 9]
 
 # Modified A : [1, 5, 6, 8, 9]
+
+
+def merge_arr(arr1, arr2):
+    merged = []
+    for num1 in arr2:
+        for i, num2 in enumerate(arr1):
+            if num1 > num2 and num1 <= arr1[i + 1]:
+                merged.append(num1)
+            else:
+                merged.append(num2)
+    return merged
+
+
+print(merge_arr([1, 5, 8], [6, 9]))
