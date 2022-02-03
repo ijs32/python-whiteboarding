1d9a011641a3b65ba1ab004e30d82a7bf42b0bd0
Ian S <iansilber32@gmail.com>
2022-02-02 19:48:41 -0500

    data-transformation whiteboarding

--- algo-ladder/Data-Transformations/book_organizer.py
+++ algo-ladder/Data-Transformations/book_organizer.py
@@ -1,14 +1,14 @@
 # Given a list of books provided in this format:
 
-# [
-# {title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
-# {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-# {title: "1984", author: "George Orwell", year: 1949 },
-# {title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
-# {title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
-# {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-# {title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
-# ]
+books = [
+    {"title": "The Lord of the Rings", "author": "J. R. R. Tolkien", "year": 1954},
+    {"title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960},
+    {"title": "1984", "author": "George Orwell", "year": 1949},
+    {"title": "Go Set a Watchman", "author": "Harper Lee", "year": 2015},
+    {"title": "The Hobbit", "author": "J. R. R. Tolkien", "year": 1937},
+    {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925},
+    {"title": "The Two Towers", "author": "J. R. R. Tolkien", "year": 1954}
+]
 
 # return the data in this new author-centric format:
 
@@ -28,3 +28,17 @@
 # {title: "The Great Gatsby", year: 1925 }
 # ]
 # }
+
+
+def book_organizer(books):
+    authors = {}
+    for book in books:
+        authors[book["author"]] = [] # creates an array for each author
+    for book in books:
+        if book["author"] in authors: # if author matches hash author do following
+            temp = {"title": book["title"], "year": book["year"]}
+            authors[book["author"]].append(temp)
+    return authors
+
+
+print(book_organizer(books))
--- algo-ladder/Data-Transformations/complete_data_II.py
+++ algo-ladder/Data-Transformations/complete_data_II.py
@@ -2,21 +2,22 @@
 
 # For example, given this array of posts (note that the submitted_by is an id number):
 
-# [
-# {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
-# {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
-# {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
-# {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
-# ]
+posts = [
+    {"title": 'Me Eating Pizza', "submitted_by": 231, "likes": 1549},
+    {"title": 'i never knew how cool i was until now',
+        "submitted_by": 989, "likes": 3},
+    {"title": 'best selfie evar!!!', "submitted_by": 111, "likes": 1092},
+    {"title": 'Mondays are the worst', "submitted_by": 403, "likes": 644}
+]
 
 # And this array of users:
 
-# [
-# {user_id: 403, name: "Aunty Em"},
-# {user_id: 231, name: "Joelle P."},
-# {user_id: 989, name: "Lyndon Johnson"},
-# {user_id: 111, name: "Patti Q."},
-# ]
+users = [
+    {"user_id": 403, "name": "Aunty Em"},
+    {"user_id": 231, "name": "Joelle P."},
+    {"user_id": 989, "name": "Lyndon Johnson"},
+    {"user_id": 111, "name": "Patti Q."},
+]
 
 # Return the array of posts as follows:
 
@@ -26,3 +27,15 @@
 # {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
 # {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
 # ]
+
+
+def format_posts(posts, users):
+    users_formatted = {}
+    for user in users:
+        users_formatted[user["user_id"]] = user["name"]
+    for post in posts:
+        post["submitted_by"] = users_formatted[post["submitted_by"]]
+    return posts
+
+
+print(format_posts(posts, users))
